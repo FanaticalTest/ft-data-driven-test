4a2f98149e7910899240ed42234af1eea56a1e62
Samy Kacem <kacems@ziridis.com>
2018-10-06 16:47:22 +0200

    initial load

---
+++ src/main/java/com/fanaticaltest/ftdatadriventest/FtDataDrivenTestApplication.java
@@ -0,0 +1,12 @@
+package com.fanaticaltest.ftdatadriventest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtDataDrivenTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtDataDrivenTestApplication.class, args);
+	}
+}
